85b97eafc18d771619a2981dc282e66bc435cd3d
Victor Gunnarsson <victor@educar.se>
2021-10-05 17:48:03 +0200

    update

---
+++ build/precache-manifest.db5d1de78efd2155e97f4a095e5019e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1dd4bdeed5e841c29cf8470bb8c1d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7b81879f9b45f47e65e",
+    "url": "./static/css/main.ab81c26c.chunk.css"
+  },
+  {
+    "revision": "c5dc522e65ce35779c35",
+    "url": "./static/js/2.d2a25449.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d2a25449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b81879f9b45f47e65e",
+    "url": "./static/js/main.9666ed16.chunk.js"
+  },
+  {
+    "revision": "ae93fa582b1e7949d1c7",
+    "url": "./static/js/runtime-main.1ced40ea.js"
+  }
+]);
